04f503468cb264dea88ec6a5cfd7ef3f5424c199
thanhtamqn29 <144640484+thanhtamqn29@users.noreply.github.com>
2024-03-26 17:33:57 +0700

    a

--- src/entities/productReviews.entity.ts
+++ src/entities/productReviews.entity.ts
@@ -5,28 +5,28 @@ import { User } from "./User.entity";
 
 @Entity('productReviews')
 export class ProductReview extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    @IsInt({ message: 'Rating must be an integer' })
-    @Min(0, { message: 'Rating must be greater than or equal to 0' })
-    @Max(5, { message: 'Rating must be less than or equal to 5' })
-    rating: number;
+  @Column()
+  @IsInt({ message: 'Rating must be an integer' })
+  @Min(0, { message: 'Rating must be greater than or equal to 0' })
+  @Max(5, { message: 'Rating must be less than or equal to 5' })
+  rating: number;
 
-    @Column({ type: "text" })
-    @IsNotEmpty({ message: 'Comment cannot be empty' })
-    comment: string;
-    
-    @CreateDateColumn({type:"timestamp"})
-    created_at: Date;
-    
+  @Column({ type: "text" })
+  @IsNotEmpty({ message: 'Comment cannot be empty' })
+  comment: string;
+
+  @CreateDateColumn({ type: "timestamp" })
+  created_at: Date;
+
   @ManyToOne(() => Product, product => product.reviews)
   @JoinColumn({ name: 'product_id' })
   product: Product;
 
-  @ManyToOne(() => User,user => user.productReviews)
-  @JoinColumn({name:'user_id'})
-  user:User;
-  
+  @ManyToOne(() => User, user => user.productReviews)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
 }
